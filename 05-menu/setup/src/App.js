9efd8c18a012cfc46bc9e6e8f6751c1a1c13bb82
ananana3112 <caongocanh3112@gmail.com>
2021-10-14 11:53:28 +0900

    done 4nd part

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -1,10 +1,34 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
+
+const allCatergories = ["all", ...new Set(items.map((item) => item.category))];
 
 function App() {
-  return <h2>menu project setup</h2>;
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCatergories] = useState(allCatergories);
+
+  const filterItems = (category) => {
+    if (category === "all") {
+      return setMenuItems(items);
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>Our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} {...items} />
+      </section>
+    </main>
+  );
 }
 
 export default App;
--- 05-menu/setup/src/Categories.js
+++ 05-menu/setup/src/Categories.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React from "react";
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({ categories, filterItems }) => {
+  return (
+    <div className="btn-container">
+      {categories.map((catergory, index) => {
+        return (
+          <button
+            key={index}
+            className="filter-btn"
+            onClick={() => filterItems(catergory)}
+          >
+            {catergory}
+          </button>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Categories;
